71720810e57bee0ed4d4f9dcc1070ab7062e23c3
Andrew Thal <andrew.thal@livingsocial.com>
2012-10-03 20:28:45 -0400

    Move admin things to an admin page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ HuronCarMatrix::Application.routes.draw do
   resources :flights
 
   resources :cars
+  resources :admin, :only => [:index]
+  match 'admin' => 'admin#index'
   match 'reorganize' => 'cars#reorganize'
   match 'car_matrix' => 'cars#car_matrix'
   root :to => 'cars#car_matrix'
