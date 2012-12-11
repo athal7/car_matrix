f262e68e0c1ae0d933b823777efa55b3bce25888
Andrew Thal <andrew.thal@livingsocial.com>
2012-12-10 23:24:19 -0500

    Add ability to create repeating flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -10,6 +10,7 @@ class FlightsController < ApplicationController
 
   def new
     @flight = Flight.new
+    @new = true
   end
 
   def edit
@@ -17,9 +18,26 @@ class FlightsController < ApplicationController
   end
 
   def create
-    @flight = Flight.new(params[:flight])
+    date_params = params[:flight].select{|k,v| k.match(/flight_time/) }
+    original_flight_time = DateTime.new(date_params["flight_time(1i)"].to_i, 
+                            date_params["flight_time(2i)"].to_i,
+                            date_params["flight_time(3i)"].to_i,
+                            date_params["flight_time(4i)"].to_i,
+                            date_params["flight_time(5i)"].to_i)
+    new_flights = []
+    num_weeks = params[:repeating].to_i > 0 ? params[:repeating].to_i : 1
+    num_weeks.times do |i|
+      flight_time = original_flight_time + i.weeks
+      new_flights << Flight.new(
+        :airline => params[:flight][:airline],
+        :flight_number => params[:flight][:flight_number],
+        :traveler_name => params[:flight][:traveler_name],
+        :flight_time => flight_time
+      )
+    end
 
-    if @flight.save
+    if new_flights.all?(&:valid?)
+      new_flights.each(&:save)
       Car.reorganize
       redirect_to root_url, notice: 'Flight was successfully created.'
     else
