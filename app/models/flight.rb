0e320f06cf9e5a902736f5c678d8dc86cd5713f1
Andrew Thal <andrew.thal@livingsocial.com>
2013-01-01 15:13:50 -0500

    Add tuesday and wednesday for reorganization

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -15,7 +15,7 @@ class Flight < ActiveRecord::Base
   end
 
   def arrival?
-    flight_time.monday?
+    flight_time.monday? || flight_time.tuesday? || flight_time.wednesday?
   end
 
   def eligible_for_shuttle?
